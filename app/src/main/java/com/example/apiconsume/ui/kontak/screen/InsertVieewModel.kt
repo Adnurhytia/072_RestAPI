c3b7745b3a9c90d5f1a0f57609c2eb55c0319538
Adnurhytia <adindanurhayati30@gmail.com>
2023-12-27 20:37:05 +0700

    Menambahkan class viewmodel

--- app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertVieewModel.kt
+++ app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertVieewModel.kt
@@ -1,2 +1,8 @@
 package com.example.apiconsume.ui.kontak.screen
 
+import androidx.lifecycle.ViewModel
+import com.example.consumeapi.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
+
+}
