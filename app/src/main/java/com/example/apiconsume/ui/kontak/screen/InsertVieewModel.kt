dae3fef78f5a47a0b2875baf922bb4cfae79f3bf
Adnurhytia <adindanurhayati30@gmail.com>
2023-12-27 20:40:47 +0700

    Menambahkan variabel dan function pd InsertViewModel

--- app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertVieewModel.kt
+++ app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertVieewModel.kt
@@ -1,8 +1,14 @@
 package com.example.apiconsume.ui.kontak.screen
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.consumeapi.repository.KontakRepository
+import com.example.consumeapi.ui.home.viewmodel.InsertUiState
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
-
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+    fun updateInsertKontakState(insertUiState: InsertUiState){
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 }
