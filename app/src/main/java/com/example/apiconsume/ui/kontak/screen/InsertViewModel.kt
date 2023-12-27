3734cc5a73b85493a870a76818a1c9392023fc64
Adnurhytia <adindanurhayati30@gmail.com>
2023-12-27 20:45:21 +0700

    Menambahkan variabel pd class InsertUiState

--- app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertViewModel.kt
+++ app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertViewModel.kt
@@ -2,9 +2,11 @@ package com.example.apiconsume.ui.kontak.screen
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.consumeapi.repository.KontakRepository
+import com.example.consumeapi.ui.home.viewmodel.InsertUiEvent
 import com.example.consumeapi.ui.home.viewmodel.InsertUiState
 import com.example.consumeapi.ui.home.viewmodel.toKontak
 import kotlinx.coroutines.launch
@@ -26,3 +28,7 @@ class InsertViewModel(private val kontakRepository: KontakRepository) : ViewMode
         }
     }
 }
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
