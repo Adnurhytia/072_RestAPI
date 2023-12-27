f0c0abd76eff563fde3674c5de8c2a3a073b962e
Adnurhytia <adindanurhayati30@gmail.com>
2023-12-27 20:43:10 +0700

    Menambahkan function pd insert kontak

--- app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertVieewModel.kt
+++ app/src/main/java/com/example/apiconsume/ui/kontak/screen/InsertVieewModel.kt
@@ -1,14 +1,28 @@
 package com.example.apiconsume.ui.kontak.screen
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.consumeapi.repository.KontakRepository
 import com.example.consumeapi.ui.home.viewmodel.InsertUiState
+import com.example.consumeapi.ui.home.viewmodel.toKontak
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){
     var insertKontakState by mutableStateOf(InsertUiState())
         private set
     fun updateInsertKontakState(insertUiState: InsertUiState){
         insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak(){
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
     }
 }
